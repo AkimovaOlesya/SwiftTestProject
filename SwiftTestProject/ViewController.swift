cdf84fa4f157e6d5d539f8dd1840ad07e38a465c
o.akimova <o.akimova@movavi.com>
2021-11-11 12:29:42 +0700

    Initial Commit

---
+++ SwiftTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftTestProject
+//
+//  Created by o.akimova on 11.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
